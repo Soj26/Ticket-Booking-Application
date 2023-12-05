617c4533fcb6426ae477e29ad30154e538babfb5
loydrojo <130616604+loydrojo@users.noreply.github.com>
2023-12-04 20:53:58 -0500

    Partially Fixed the Registration

--- src/main/java/ca/sheridancollege/alagao/controllers/HomeController.java
+++ src/main/java/ca/sheridancollege/alagao/controllers/HomeController.java
@@ -1,9 +1,14 @@
 package ca.sheridancollege.alagao.controllers;
 
+import ca.sheridancollege.alagao.beans.User;
 import ca.sheridancollege.alagao.database.DatabaseAccess;
 import ca.sheridancollege.alagao.database.TicketDatabase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -13,4 +18,41 @@ public class HomeController {
     @Autowired
     private TicketDatabase tDa;
 
+    @GetMapping("/register")
+    public String getRegistrationForm(Model model) {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegister(@RequestParam String email,
+                                  @RequestParam String password,
+                                  @RequestParam String confirmPassword,
+                                  @RequestParam String name,
+                                  @RequestParam(required = false) String role,
+                                  Model model) {
+        if (!password.equals(confirmPassword)) {
+            model.addAttribute("error", "Passwords do not match");
+            return "register";
+        }
+
+        try {
+            database.addUser(email, password, name);
+            Long userId = database.findUserAccount(email).getUserID();
+
+            // Set the role to "User" for regular users
+            if (!model.getAttribute("role").equals("ADMIN")) {
+                role = "USER";
+            }
+
+            // Assuming getRoleId method is implemented in DatabaseAccess
+            Long roleId = database.getRoleId("ROLE_" + role.toUpperCase());
+            database.addRole(userId, roleId);
+
+            return "redirect:/login";
+        } catch (Exception e) {
+            model.addAttribute("error", "Registration failed: " + e.getMessage());
+            return "register";
+        }
+    }
 }
+
--- src/main/java/ca/sheridancollege/alagao/database/DatabaseAccess.java
+++ src/main/java/ca/sheridancollege/alagao/database/DatabaseAccess.java
@@ -4,6 +4,7 @@ import ca.sheridancollege.alagao.beans.User;
 import ca.sheridancollege.alagao.beans.Ticket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -52,6 +53,38 @@ public class DatabaseAccess {
 
         jdbc.update(query, parameters);
     }
+    public void addRole(Long userId, Long roleId) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("userID", userId);
+        params.addValue("roleId", roleId);
 
+        String query = "INSERT INTO user_role (userID, roleId) VALUES (:userID, :roleId)";
+
+        try {
+            jdbc.update(query, params);
+        } catch (DuplicateKeyException e) {
+            // Handle the case where the role assignment already exists
+            // This is important to avoid trying to insert a duplicate role assignment
+            System.out.println("Role already assigned to user: " + e.getMessage());
+            // Depending on your application's requirements, you might want to throw an exception or just log the error
+        } catch (Exception e) {
+            // Handle other exceptions such as connectivity issues with the database
+            System.out.println("Error in adding role to user: " + e.getMessage());
+            // Again, depending on your requirements, either handle the exception or rethrow it
+        }
+    }
+    public Long getRoleId(String roleName) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("roleName", roleName);
+
+        String query = "SELECT roleId FROM sec_role WHERE roleName = :roleName";
+
+        try {
+            return jdbc.queryForObject(query, params, Long.class);
+        } catch (Exception e) {
+            System.out.println("Error retrieving role ID: " + e.getMessage());
+            return null; // Or handle the exception as appropriate for your application
+        }
+    }
 
 }
