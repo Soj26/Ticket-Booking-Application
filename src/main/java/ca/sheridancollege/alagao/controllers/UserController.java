1d6dbbb9c2900070c3aa2186c8984c8d578077a9
loydrojo <130616604+loydrojo@users.noreply.github.com>
2023-12-07 07:04:43 -0500

    working buy

--- src/main/java/ca/sheridancollege/alagao/controllers/UserController.java
+++ src/main/java/ca/sheridancollege/alagao/controllers/UserController.java
@@ -1,64 +1,72 @@
-    package ca.sheridancollege.alagao.controllers;
+package ca.sheridancollege.alagao.controllers;
 
-    import ca.sheridancollege.alagao.beans.Ticket;
-    import ca.sheridancollege.alagao.beans.User;
-    import ca.sheridancollege.alagao.database.DatabaseAccess;
-    import ca.sheridancollege.alagao.database.TicketDatabase;
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.security.core.Authentication;
-    import org.springframework.stereotype.Controller;
-    import org.springframework.ui.Model;
-    import org.springframework.web.bind.annotation.*;
+import ca.sheridancollege.alagao.beans.Ticket;
+import ca.sheridancollege.alagao.beans.User;
+import ca.sheridancollege.alagao.database.DatabaseAccess;
+import ca.sheridancollege.alagao.database.TicketDatabase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-    import java.math.BigDecimal;
-    import java.security.Principal;
-    import java.util.List;
-    @Controller
-    @RequestMapping("secured/user/")
-    public class UserController {
+import java.math.BigDecimal;
+import java.security.Principal;
+import java.util.List;
 
-        @Autowired
-        private DatabaseAccess database;
+@Controller
+@RequestMapping("/secured/user")
+public class UserController {
 
-        @Autowired
-        private TicketDatabase ticketDatabase;
+    @Autowired
+    private DatabaseAccess database;
 
-        @GetMapping("/userIndex")
-        public String userDashboard(Model model, Principal principal) {
-            String email = principal.getName();
-            User user = database.findUserAccount(email);
-            List<Ticket> availableTickets = ticketDatabase.findAvailableTickets();
+    @Autowired
+    private TicketDatabase ticketDatabase;
 
-            model.addAttribute("user", user);
-            model.addAttribute("availableTickets", availableTickets);
-            return "secured/user/userIndex";
-        }
+    @GetMapping("/userIndex")
+    public String userDashboard(Model model, Principal principal) {
+        String email = principal.getName();
+        User user = database.findUserAccount(email);
+        List<Ticket> availableTickets = ticketDatabase.findAvailableTickets();
 
-
-        @PostMapping("/buyTicket/{ticketId}")
-        public String buyTicket(@PathVariable Long ticketId, Principal principal, Model model) {
-            User user = database.findUserAccountByEmail(principal.getName());
-            Ticket ticket = ticketDatabase.findTicketById(ticketId);
-
-            if (ticket != null && ticket.isAvailable() && user.getBalance().compareTo(ticket.getPrice()) >= 0) {
-
-                BigDecimal newBalance = user.getBalance().subtract(ticket.getPrice());
-                database.updateUserBalance(user.getEmail(), newBalance);
-
-
-                int newNumberOfSeats = ticket.getNumberOfSeats() - 1;
-                ticket.setNumberOfSeats(newNumberOfSeats);
-                if (newNumberOfSeats == 0) {
-                    ticket.setAvailable(false);
-                }
-                ticketDatabase.updateTicket(ticket);
-
-                model.addAttribute("success", "Ticket purchased successfully!");
-                return "secured/user/userIndex";
-            } else {
-                model.addAttribute("error", "Cannot purchase ticket.");
-                return "secured/user/userIndex";
-            }
-        }
+        model.addAttribute("user", user);
+        model.addAttribute("availableTickets", availableTickets);
+        return "secured/user/userIndex";
     }
 
+    @PostMapping("/buyTicket/{ticketId}")
+    public String buyTicket(@PathVariable Long ticketId,
+                            @RequestParam("quantity") int quantity,
+                            Principal principal,
+                            RedirectAttributes redirectAttributes) {
+        User user = database.findUserAccountByEmail(principal.getName());
+        Ticket ticket = ticketDatabase.findTicketById(ticketId);
+
+        if (ticket != null && ticket.isAvailable()) {
+            BigDecimal totalCost = ticket.getPrice().multiply(new BigDecimal(quantity));
+            if (user.getBalance().compareTo(totalCost) >= 0) {
+                BigDecimal newBalance = user.getBalance().subtract(totalCost);
+                database.updateUserBalance(user.getEmail(), newBalance);
+
+                int newNumberOfSeats = ticket.getNumberOfSeats() - quantity;
+                ticket.setNumberOfSeats(newNumberOfSeats);
+                ticket.setAvailable(newNumberOfSeats > 0);
+                ticketDatabase.updateTicket(ticket);
+
+                redirectAttributes.addFlashAttribute("successMessage", "Transaction successful!");
+                redirectAttributes.addFlashAttribute("totalCost", totalCost);
+                redirectAttributes.addFlashAttribute("newBalance", newBalance);
+                return "redirect:/secured/user/transactionSummary";
+            }
+        }
+
+        redirectAttributes.addFlashAttribute("error", "Cannot purchase ticket. Not enough seats or insufficient balance.");
+        return "redirect:/secured/user/userIndex";
+    }
+
+    @GetMapping("/transactionSummary")
+    public String transactionSummary(Model model) {
+        return "secured/user/transactionSummary";
+    }
+}
